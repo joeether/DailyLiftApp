08fc326df8c4a8241c8e211fc6b53964f5046202
Joey <collinjonahsdad@gmail.com>
2026-05-20 08:50:06 -0500

    Added Service worker Registration

--- DailyLiftApp/DailyLiftApp.Client/wwwroot/firebase-messaging-sw.js
+++ DailyLiftApp/DailyLiftApp.Client/wwwroot/firebase-messaging-sw.js
@@ -2,23 +2,23 @@ importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaJrPJXYikMHJQvWp1mBd9qMtjzzpe-68",
-  authDomain: "dailyliftapp-aa3e1.firebaseapp.com",
-  projectId: "dailyliftapp-aa3e1",
-  storageBucket: "dailyliftapp-aa3e1.firebasestorage.app",
-  messagingSenderId: "992258604859",
-  appId: "1:992258604859:web:1b1acc7cd89cb9b093011e"
+    apiKey: "AIzaSyDaJrPJXYikMHJQvWp1mBd9qMtjzzpe-68",
+    authDomain: "dailyliftapp-aa3e1.firebaseapp.com",
+    projectId: "dailyliftapp-aa3e1",
+    storageBucket: "dailyliftapp-aa3e1.firebasestorage.app",
+    messagingSenderId: "992258604859",
+    appId: "1:992258604859:web:1b1acc7cd89cb9b093011e"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message:', payload);
-  const notificationTitle = payload.notification?.title || "Daily Lift";
-  const notificationOptions = {
-    body: payload.notification?.body || "Time for today's wisdom 💪",
-    icon: "/icon-192.png" // make sure you have this in wwwroot
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    console.log('Received background message:', payload);
+    const notificationTitle = payload.notification?.title || "Daily Lift";
+    const notificationOptions = {
+        body: payload.notification?.body || "Time for today's wisdom 💪",
+        icon: "/icon-192.png"
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
